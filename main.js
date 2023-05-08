6fa3243def7875c8bea557a4cf5f79bd9b348630
trikiiahmed <triki1059@gmail.com>
2023-05-08 16:07:58 +0100

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+var player1={name:'rahma',lastName:'Ben slimane',height:'1.88',playerposition:'setter',playerNumber:3}
+var player2={name:'Ele',lastName:'essid',height:'1.83',playerPosition:'opposite attack',playerNumber:17}
+var player3={name:'Sondes',lastName:'Sassi',height:'1.70',playerposition:'setter',playerNumber:18}
+var player4={name:'tasnim',lastName:'bechiekh',height:'1.78',playerposition:'attacking receiver',playerNumber:12}
+var player5={name:'ele',lastName:'rezgui',height:'1.89',playerposition:'attacking receiver',playerNumber:7}
+var player6={name:'Nermine',lastName:'bHamza',height:'1.75',playerposition:'middle blocker',playerNumber:1}
+var player7={name:'Yasmine',lastName:'salhi',height:'1.77',playerposition:'middle blocker',playerNumber:2}
+var player8={name:'oumaima',lastName:'jalel',height:'1.73',playerposition:'libero',playerNumber:20}
+
+
